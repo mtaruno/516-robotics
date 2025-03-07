ae91d2939e397d10445bddf36b38dd508bee2fc7
mtaruno <mtaruno@uw.edu>
2025-03-06 17:54:58 -0800

    clean up

--- lab7/lab_quaternion/build/lab_quaternion/CMakeFiles/lab_quaternion_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lab_quaternion_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lab_quaternion_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lab7/lab_quaternion/build/lab_quaternion/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/mtaruno@netid.washington.edu/TECHIN516/lab7/src/lab_quaternion/install/lab_quaternion'
